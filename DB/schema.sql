ac4287b52ff18bdff57960f57e981734d4b2acb1
nguyen0220 <teemee202@gmail.com>
2025-06-03 12:21:12 +0900

    db
    Signed-off-by: nguyen0220 <teemee202@gmail.com>

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,9 +1,10 @@
--- 데이터베이스 생성
+-- 1. Create database
+DROP DATABASE messageprogram;
 CREATE DATABASE IF NOT EXISTS MessageProgram;
 USE MessageProgram;
 
--- USER 테이블 생성
-CREATE TABLE IF NOT EXISTS USER (
+-- 2. USER table
+CREATE TABLE IF NOT EXISTS `user` (
     user_id VARCHAR(20) PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     password_hash VARCHAR(100) NOT NULL,
@@ -11,9 +12,30 @@ CREATE TABLE IF NOT EXISTS USER (
     work_status VARCHAR(50) DEFAULT '업무 중',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+    );
 
--- SESSION 테이블 생성
+CREATE TABLE IF NOT EXISTS attendance (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(20) NOT NULL,
+    check_in_time DATETIME,
+    check_out_time DATETIME,
+    work_time_total INT,
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id) ON DELETE CASCADE
+    );
+
+
+CREATE TABLE IF NOT EXISTS AttendanceEditRequest (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     user_id VARCHAR(20) NOT NULL,
+    attendance_date DATE NOT NULL,
+    requested_check_in TIME,
+    requested_check_out TIME,
+    reason TEXT NOT NULL,
+    requested_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(20) DEFAULT 'PENDING',
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id) ON DELETE CASCADE
+    );
+
 CREATE TABLE IF NOT EXISTS SESSION (
     session_id VARCHAR(128) PRIMARY KEY,
     user_id VARCHAR(50) NOT NULL,
@@ -21,7 +43,7 @@ CREATE TABLE IF NOT EXISTS SESSION (
     expired_at DATETIME NOT NULL,
     is_active BOOLEAN DEFAULT TRUE,
     FOREIGN KEY (user_id) REFERENCES USER(user_id) ON DELETE CASCADE
-);
+    );
 
 -- MESSAGE 테이블 생성
 CREATE TABLE IF NOT EXISTS MESSAGE (
@@ -31,4 +53,34 @@ CREATE TABLE IF NOT EXISTS MESSAGE (
     sent DATETIME NOT NULL,
     is_system TINYINT(1),
     content TEXT
-);
+    );
+
+CREATE TABLE IF NOT EXISTS CHATROOM (
+    chatroom_id INT AUTO_INCREMENT PRIMARY KEY,
+    parentroom_id INT DEFAULT NULL,
+    closed BOOLEAN DEFAULT FALSE,
+    is_private BOOLEAN DEFAULT FALSE,
+    title VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (parentroom_id) REFERENCES CHATROOM(chatroom_id)
+    );
+
+CREATE TABLE IF NOT EXISTS CHATROOM_PARTICIPANT(
+    chatroom_id INT NOT NULL,
+    user_id VARCHAR(20) NOT NULL,
+    PRIMARY KEY (chatroom_id, user_id),
+    FOREIGN KEY (chatroom_id) REFERENCES CHATROOM(chatroom_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES USER(user_id) ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS FRIEND (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(30) NOT NULL,
+    friend_id VARCHAR(30) NOT NULL,
+    is_blocked BOOLEAN DEFAULT FALSE,
+    is_pending BOOLEAN DEFAULT TRUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(user_id),
+    CONSTRAINT fk_friend FOREIGN KEY (friend_id) REFERENCES user(user_id)
+    );
+
