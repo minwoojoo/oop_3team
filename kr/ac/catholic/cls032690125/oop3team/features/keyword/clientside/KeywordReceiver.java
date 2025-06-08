f3a0ff9bbc0591224f976f081603ccb681735c37
Hyun-ku Lee <dev@hklee.me>
2025-06-08 13:37:23 +0900

    temp
    Signed-off-by: Hyun-ku Lee <dev@hklee.me>

--- kr/ac/catholic/cls032690125/oop3team/features/keyword/clientside/KeywordReceiver.java
+++ kr/ac/catholic/cls032690125/oop3team/features/keyword/clientside/KeywordReceiver.java
@@ -1,4 +1,22 @@
 package kr.ac.catholic.cls032690125.oop3team.features.keyword.clientside;
 
-public class KeywordReceiver {
+import kr.ac.catholic.cls032690125.oop3team.client.Client;
+import kr.ac.catholic.cls032690125.oop3team.client.structs.ClientResponseHandler;
+import kr.ac.catholic.cls032690125.oop3team.client.structs.ClientResponseListener;
+import kr.ac.catholic.cls032690125.oop3team.features.chat.shared.SMessageBroadcastPacket;
+import kr.ac.catholic.cls032690125.oop3team.models.Keyword;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeywordReceiver extends ClientResponseListener {
+    private List<Keyword> keywords = new ArrayList<>();
+
+    public KeywordReceiver(Client client) {
+        super(client);
+    }
+
+    @ClientResponseHandler(SMessageBroadcastPacket.class)
+    public void onSMessageBroadcastPacket(SMessageBroadcastPacket packet) {
+    }
 }
